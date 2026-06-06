70ee3f5ee5adf150932a202be9479f9641922f2d
Saravana vignesh <saravanavignesh6@gmail.com>
2026-06-06 19:46:22 +0530

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int ans=Integer.MIN_VALUE, prefix=0, suffix=0;
+        for(int i=0;i<nums.length;i++){
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+
+            prefix = prefix*nums[i];
+            suffix = suffix*nums[nums.length-i-1];
+            ans = Math.max(ans,Math.max(prefix,suffix));
+        }
+        return ans;
+    }
+}
