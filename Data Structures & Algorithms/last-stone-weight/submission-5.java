5595f226ebed095d02e6db14b881f29dfc00ba47
Saravana vignesh <saravanavignesh6@gmail.com>
2026-06-23 18:35:03 +0530

    Add: last-stone-weight - submission-5

---
+++ Data Structures & Algorithms/last-stone-weight/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> b-a);
+
+        for(int i=0;i<stones.length;i++){
+            pq.offer(stones[i]);
+        }
+        while(pq.size()>1){
+            int x = pq.poll();
+            int y = pq.poll();
+            if(x!=y){
+                pq.offer(Math.abs(x-y));
+            }        
+        }
+        return pq.isEmpty() ? 0 : pq.poll(); 
+    }
+}
